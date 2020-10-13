b5425ed7e24617b2a0363fb7da9e0ae3d8f5f571
ArifComek <acomek@gmail.com>
2020-10-13 21:25:32 +0200

    Update TodayIsTuesday.java

--- src/test/java/TodayIsTuesday.java
+++ src/test/java/TodayIsTuesday.java
@@ -2,5 +2,7 @@ public class TodayIsTuesday {
     public static void main(String[] args) {
         System.out.println("Hello our awesome group 14");
         System.out.println("One more hello!");
+        //========
+        System.out.println("New project!");
     }
 }
