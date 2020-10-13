1fdd65d4e344d45cee12c996f26b8e7699874ac7
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-13 20:28:35 +0200

    One more line added.

--- src/test/java/TodayIsTuesday.java
+++ src/test/java/TodayIsTuesday.java
@@ -1,5 +1,6 @@
 public class TodayIsTuesday {
     public static void main(String[] args) {
         System.out.println("Hello our awesome group 14");
+        System.out.println("One more hello!");
     }
 }
